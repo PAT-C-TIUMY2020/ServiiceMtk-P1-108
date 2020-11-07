a6a274bddfea7b9f439e826cb58af7d141cb0cd1
KinanthieMPutrie <kinanthieputrie@gmail.com>
2020-11-07 14:32:59 +0800

    Add project files.

---
+++ ServiceMtk-P2-20180140108/IMatematika.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceMtk_P2_20180140108
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IMatematika
+    {
+        [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas yang melambangkan exception
+        /* output->*/
+        int Tambah(int a, int b); //method
+
+        [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas yang melambangkan exception
+        int Kurang(int a, int b); //input
+
+        [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas yang melambangkan exception
+        int Kali(int a, int b);
+
+        [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas yang melambangkan exception
+        int Bagi(int a, int b);
+
+        [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas yang melambangkan exception
+        Koordinat TKoordinat(Koordinat a, Koordinat b); //object dari class
+
+    }
+
+    [DataContract]
+    class MathFault
+    {
+        [DataMember]
+        public string Kode { get; set; }
+        [DataMember]
+        public string Pesan { get; set; }
+    }
+    public class Koordinat
+    {
+        private int _x, _y; //atribut
+        [DataMember]//membuka koordinat, x,y
+        public int X //properties
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+        [DataMember]
+        public int Y //properties
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+    }
+}
